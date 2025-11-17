bca4147884565099fd39f6dd714dcc49143c2747
lowsugarcoke <jack20499@gmail.com>
2025-11-16 20:24:15 -0500

    fix: hot fix profile edit page

--- src/app/profile/[pageUserId]/edit/page.tsx
+++ src/app/profile/[pageUserId]/edit/page.tsx
@@ -295,36 +295,40 @@ export default function Page({
   const onSubmit = async (values: ProfileFormValues) => {
     await updateProfile(values);
 
-    await upsertMentorExperience(ExperienceType.WORK, true, {
-      id: 1,
-      category: ExperienceType.WORK,
-      mentor_experiences_metadata: {
-        data: values.work_experiences.map((item) => ({
-          job: item.job,
-          company: item.company,
-          jobPeriodStart: item.jobPeriodStart,
-          jobPeriodEnd: item.jobPeriodEnd,
-          industry: item.industry,
-          jobLocation: item.jobLocation,
-          description: item.description,
-        })),
-      },
-      order: 1,
-    });
+    if (values.work_experiences?.length > 0) {
+      await upsertMentorExperience(ExperienceType.WORK, true, {
+        id: 1,
+        category: ExperienceType.WORK,
+        mentor_experiences_metadata: {
+          data: values.work_experiences.map((item) => ({
+            job: item.job,
+            company: item.company,
+            jobPeriodStart: item.jobPeriodStart,
+            jobPeriodEnd: item.jobPeriodEnd,
+            industry: item.industry,
+            jobLocation: item.jobLocation,
+            description: item.description,
+          })),
+        },
+        order: 1,
+      });
+    }
 
-    await upsertMentorExperience(ExperienceType.EDUCATION, true, {
-      id: 2,
-      category: ExperienceType.EDUCATION,
-      mentor_experiences_metadata: {
-        data: values.educations.map((item) => ({
-          school: item.school,
-          subject: item.subject,
-          educationPeriodStart: item.educationPeriodStart,
-          educationPeriodEnd: item.educationPeriodEnd,
-        })),
-      },
-      order: 2,
-    });
+    if (values.educations?.length > 0) {
+      await upsertMentorExperience(ExperienceType.EDUCATION, true, {
+        id: 2,
+        category: ExperienceType.EDUCATION,
+        mentor_experiences_metadata: {
+          data: values.educations.map((item) => ({
+            school: item.school,
+            subject: item.subject,
+            educationPeriodStart: item.educationPeriodStart,
+            educationPeriodEnd: item.educationPeriodEnd,
+          })),
+        },
+        order: 2,
+      });
+    }
 
     const links = [
       values.linkedin,
@@ -333,30 +337,34 @@ export default function Page({
       values.twitter,
       values.youtube,
       values.website,
-    ];
+    ].filter((l) => l && l.url);
 
-    await upsertMentorExperience(ExperienceType.LINK, true, {
-      id: 3,
-      category: ExperienceType.LINK,
-      mentor_experiences_metadata: {
-        data: links.map((link) => ({
-          platform: link.platform,
-          url: link.url,
-        })),
-      },
-      order: 3,
-    });
+    if (links.length > 0) {
+      await upsertMentorExperience(ExperienceType.LINK, true, {
+        id: 3,
+        category: ExperienceType.LINK,
+        mentor_experiences_metadata: {
+          data: links.map((link) => ({
+            platform: link.platform,
+            url: link.url,
+          })),
+        },
+        order: 3,
+      });
+    }
 
-    await upsertMentorExperience(ExperienceType.WHAT_I_OFFER, true, {
-      id: 4,
-      category: ExperienceType.WHAT_I_OFFER,
-      mentor_experiences_metadata: {
-        data: values.what_i_offer.map((item) => ({
-          subject_group: item,
-        })),
-      },
-      order: 4,
-    });
+    if (values.what_i_offer?.length > 0) {
+      await upsertMentorExperience(ExperienceType.WHAT_I_OFFER, true, {
+        id: 4,
+        category: ExperienceType.WHAT_I_OFFER,
+        mentor_experiences_metadata: {
+          data: values.what_i_offer.map((item) => ({
+            subject_group: item,
+          })),
+        },
+        order: 4,
+      });
+    }
 
     if (isMentorOnboarding) {
       router.push('/profile/card');
