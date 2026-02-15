f809e8bd3c5d6ae61783e04b0b8a9f6d372695cf
lowsugarcoke <jack20499@gmail.com>
2026-02-15 15:22:13 +0800

    feat: seperate mentee/mentor page

--- src/app/reservation/mentor/ui.tsx
+++ src/app/reservation/mentor/ui.tsx
@@ -9,7 +9,7 @@ export default function ReservationPresentation(
     <div className="flex min-h-screen justify-center">
       <div className="w-full max-w-[90%] overflow-hidden rounded-2xl md:max-w-[800px]">
         <div className="font-roboto mx-auto mb-6 h-[42px] w-[251px] text-center text-[36px] font-semibold leading-[100%] tracking-[0%] text-[#1D1B20]">
-          我的預約
+          擔任導師
         </div>
         <ReservationTabs {...props} />
       </div>
