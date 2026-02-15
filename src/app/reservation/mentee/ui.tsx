46e07cf15720f050e98a75ed57e9a1c3076f3b70
LowSugarCoke <jack20499@gmail.com>
2026-02-15 15:27:48 +0800

    feat: seperate mentee/mentor page (#292)

---
+++ src/app/reservation/mentee/ui.tsx
@@ -0,0 +1,18 @@
+import ReservationTabs, { type ReservationTabsProps } from './reservationTabs';
+
+export type ReservationPresentationProps = ReservationTabsProps;
+
+export default function ReservationPresentation(
+  props: ReservationPresentationProps
+) {
+  return (
+    <div className="flex min-h-screen justify-center">
+      <div className="w-full max-w-[90%] overflow-hidden rounded-2xl md:max-w-[800px]">
+        <div className="font-roboto mx-auto mb-6 h-[42px] w-[251px] text-center text-[36px] font-semibold leading-[100%] tracking-[0%] text-[#1D1B20]">
+          預約導師
+        </div>
+        <ReservationTabs {...props} />
+      </div>
+    </div>
+  );
+}
