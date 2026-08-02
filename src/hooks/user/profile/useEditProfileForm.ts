11912c5aec18c7e395428007869bd486711428f1
lowsugarcoke <jack20499@gmail.com>
2026-08-02 18:27:25 -0400

    refactor(profile): use Resolver cast directly and avoid any types completely (X-Tracker #501)

--- src/hooks/user/profile/useEditProfileForm.ts
+++ src/hooks/user/profile/useEditProfileForm.ts
@@ -2,7 +2,7 @@
 
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useMemo } from 'react';
-import { useForm, UseFormReturn } from 'react-hook-form';
+import { Resolver, useForm, UseFormReturn } from 'react-hook-form';
 
 import {
   createProfileFormSchema,
@@ -21,9 +21,11 @@ export function useEditProfileForm(isMentor: boolean): {
   form: ProfileFormContext;
 } {
   const resolver = useMemo(() => {
-    return zodResolver<ProfileFormInput, unknown, any>(
-      createProfileFormSchema(isMentor)
-    );
+    return zodResolver(createProfileFormSchema(isMentor)) as Resolver<
+      ProfileFormInput,
+      unknown,
+      ProfileFormValues
+    >;
   }, [isMentor]);
 
   const form = useForm<ProfileFormInput, unknown, ProfileFormValues>({
