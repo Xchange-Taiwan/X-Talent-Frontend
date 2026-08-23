ce3580a4c9d3f7e6d75c2fa55399bff35ecbba4f
lowsugarcoke <jack20499@gmail.com>
2026-08-22 22:44:43 -0400

    fix(profile): ensure null initialData resolves synchronously to prevent loading flash

--- src/hooks/user/user-data/useUserProfileDto.ts
+++ src/hooks/user/user-data/useUserProfileDto.ts
@@ -144,7 +144,7 @@ export function useUserProfileDto(
       return res;
     },
     {
-      initialData: initialDataRef.current ?? undefined,
+      initialData: initialDataRef.current,
     }
   );
 
