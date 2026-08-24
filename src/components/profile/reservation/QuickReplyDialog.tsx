fc5562fb024eb7de479d11fed9646baee2e2bedf
lowsugarcoke <jack20499@gmail.com>
2026-08-24 11:41:07 -0400

    refactor(reservation): change name hover style to only show underline without color changes

--- src/components/profile/reservation/QuickReplyDialog.tsx
+++ src/components/profile/reservation/QuickReplyDialog.tsx
@@ -126,7 +126,7 @@ export function QuickReplyDialog({
                     onClick={handleProfileLinkClick}
                     className="group inline-block max-w-full rounded-sm no-underline focus-visible:ring-2 focus-visible:ring-brand-500 focus-visible:ring-offset-2 focus-visible:outline-none"
                   >
-                    <div className="truncate font-medium transition-colors hover:text-brand-500 hover:underline sm:text-base">
+                    <div className="truncate font-medium hover:underline sm:text-base">
                       {reservation.name}
                     </div>
                   </Link>
